92f22d8c133d5c1a858c5fdd11c6377cfd8bfb15
Miles Johnson <mileswjohnson@gmail.com>
2023-08-26 18:44:19 -0700

    Dont clobber store.

--- crates/cli/tests/utils.rs
+++ crates/cli/tests/utils.rs
@@ -5,5 +5,9 @@ pub fn create_espm_command(sandbox: &Path) -> starbase_sandbox::assert_cmd::Comm
     let mut cmd = create_command_with_name(sandbox, "espm");
     cmd.env("ESPM_LOG", "trace");
     cmd.env("ESPM_TEST", "true");
+    cmd.env(
+        "ESPM_ROOT",
+        sandbox.join(".espresso").to_string_lossy().to_string(),
+    );
     cmd
 }
